3a9789175902eb6e7966ec46859973a44125e0cb
developer <daverival@hotmail.com>
2022-10-13 23:47:14 +0100

    initial commit

---
+++ customer-portal/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./keycloak";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/nav.component";
+import WelcomePage from "./pages/homepage.page";
+import SecuredPage from "./pages/securedpage.page";
+import PrivateRoute from "./helpers/private-route.helpers";
+
+function App() {
+ return (
+   <div>
+     <ReactKeycloakProvider authClient={keycloak}>
+       <Nav />
+       <BrowserRouter>
+         <Routes>
+           <Route exact path="/" element={<WelcomePage />} />
+           <Route
+             path="/secured"
+             element={
+               <PrivateRoute>
+                 <SecuredPage />
+               </PrivateRoute>
+             }
+           />
+         </Routes>
+       </BrowserRouter>
+     </ReactKeycloakProvider>
+   </div>
+ );
+}
+
+export default App;
---
+++ customer-portal/src/components/footer.component.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+      <footer className='py-7 bg-[#1b252d] text-white grid grid-cols-1 place-items-center w-full'>
+        <div className="shadow-lg">
+          <ul className='flex'>
+            <li className='px-4'>Facebook</li>
+            <li className='px-4'>Instagram</li>
+            <li className='px-4'>Twitter</li>
+          </ul>
+        </div>
+      </footer>
+    )
+  }
