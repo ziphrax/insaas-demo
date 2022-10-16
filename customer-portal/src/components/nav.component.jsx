38b0ab701f67958ea43eae7360a16a53d1449691
David Jones <david.jones@coveainsurance.co.uk>
2022-10-16 22:45:12 +0100

    feat: add additional navigation

--- customer-portal/src/components/nav.component.jsx
+++ customer-portal/src/components/nav.component.jsx
@@ -1,5 +1,69 @@
 import React from "react";
 import { useKeycloak } from "@react-keycloak/web";
+import {Link} from 'react-router-dom'
+
+export const InsaaSCustomerNavigation = () => {
+  return (
+    <div>
+      <h2>InsaaS Customer</h2>
+      <ul>
+        <li>
+          <Link to="/insaas/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/insaas/affinities">Affinity Management</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export const CustomerNavigation = () => {
+  return (
+    <div>
+      <h2>Customer</h2>
+      <ul>
+        <li><Link to="/customer/account">Account</Link></li>
+        <li><Link to="/customer/claims">Claims</Link></li>
+        <li><Link to="/customer/dashboard">Dashboard</Link></li>
+        <li><Link to="/customer/policies">Policies</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export const ProductNavigation = () => {
+  return (
+    <div>
+      <h2>Product</h2>
+      <ul>
+        <li><Link to="/products">Products</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export const QuoteNavigation = () => {
+  return (
+    <div>
+      <h2>Quote</h2>
+      <ul>
+        <li><Link to="/quotes/data-capture">Data Capture</Link></li>
+        <li><Link to="/quotes/payment">Payment</Link></li>
+        <li><Link to="/quotes/price-presentation">Price Presentation</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export const SupportNavigation = () => {
+  return (
+    <div>
+      <h2>Support</h2>
+      <li><Link to="/support">Support</Link></li>
+    </div>
+  )
+}
 
 const Nav = () => {
  const { keycloak } = useKeycloak();
--- customer-portal/src/pages/homepage.page.jsx
+++ customer-portal/src/pages/homepage.page.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useKeycloak } from "@react-keycloak/web";
 import KeycloakComponent from '../components/keycloak.component';
+import {InsaaSCustomerNavigation, CustomerNavigation, ProductNavigation, QuoteNavigation, SupportNavigation } from '../components/nav.component'
+
 
 const Home = () => {
   const { keycloak } = useKeycloak();
  return (
    <div>
      <h1 className="text-green-800 text-4xl">Welcome to the Homepage</h1>
-     <pre>{JSON.stringify(keycloak,null,2)}</pre>
-     <KeycloakComponent />
+     <InsaaSCustomerNavigation />
+     <CustomerNavigation />
+     <ProductNavigation />
+     <QuoteNavigation />
+     <SupportNavigation />
+
+     {/* <pre>{JSON.stringify(keycloak,null,2)}</pre>
+     
+     <KeycloakComponent /> */}
    </div>
  );
 };
