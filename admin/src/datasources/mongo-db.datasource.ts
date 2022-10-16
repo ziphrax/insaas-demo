a9c2f16b766b27cbf6f81046db6db0a4a8a15012
David Jones <david.jones@coveainsurance.co.uk>
2022-10-16 22:32:58 +0100

    feat: add admin portal and microservice

---
+++ admin/src/datasources/mongo-db.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongoDb',
+  connector: 'mongodb',
+  url: process.env.MONGO_URL,
+  host: '',
+  port: 0,
+  user: '',
+  password: '',
+  database: '',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongoDbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongoDb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongoDb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
